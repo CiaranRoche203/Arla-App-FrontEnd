51d9c8a2efefb4eaf9bc9d027cdf2ee030102fa4
CiaranRoche203 <rocheciaran37@gmail.com>
2021-10-14 17:36:34 +0100

    Project skeleton

---
+++ src/Pages/Home.js
@@ -0,0 +1,9 @@
+function Home(){
+    return(
+        <h1>
+            Here is the Home Page
+        </h1>
+    );
+}
+
+export default Home;
---
+++ src/Pages/Register.js
@@ -0,0 +1,9 @@
+function Register(){
+    return(
+        <h1>
+            Here is the Register Page
+        </h1>
+    );
+}
+
+export default Register;
